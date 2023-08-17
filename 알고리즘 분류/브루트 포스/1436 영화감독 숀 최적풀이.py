a33365ac9195d53c67c81501d2e5dadddf6ee675
kyyneogs <oks990304@gmail.com>
2023-08-17 22:24:05 +0900

    2023-08-17

--- 알고리즘 분류/브루트 포스/1436 영화감독 숀 최적풀이.py
+++ 알고리즘 분류/브루트 포스/1436 영화감독 숀 최적풀이.py
@@ -2,6 +2,6 @@ N = int(input())
 number = 666
 count = 0
 while(count<N):
-    if '666' in str(number): count=count+1
+    if '666' in str(N): count=count+1
     number=number+1
 print(number-1)
--- 알고리즘 분류/브루트 포스/2798 블랙잭.py
+++ 알고리즘 분류/브루트 포스/2798 블랙잭.py
@@ -3,9 +3,9 @@ N, M = map(int, sys.stdin.readline().split())
 C = list(map(int, sys.stdin.readline().split()))
 
 max = 0
-for i in range(0,N-2):
-    for j in range(i+1,N-1):
-        for k in range(j+1,N):
+for i in range(0,N-2):  # 중복을 막기 위해, N-2까지 제한
+    for j in range(i+1,N-1):  # 중복을 막기 위해, i에서 항상 1 앞서있으며 N-1까지 제한
+        for k in range(j+1,N):  # 중복을 막기 위해, j에서 항상 1 앞서있음
             tmp = C[i]+C[j]+C[k]
             if tmp>max and tmp<=M: max=tmp
             if max==M: break
