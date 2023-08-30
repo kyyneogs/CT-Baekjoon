afc4d54430c9a26c71567b3ca46e64389ee3b525
kyyneogs <oks990304@gmail.com>
2023-08-31 00:15:29 +0900

    2023-08-30

--- essential/class-4/9465 스티커.py
+++ essential/class-4/9465 스티커.py
@@ -10,12 +10,14 @@ def solution():
     dp[0][0] = arr[0][0]
     dp[1][0] = arr[1][0]
 
-    dp[0][1] = dp[1][0] + arr[0][1]
-    dp[1][1] = dp[0][0] + arr[1][1]
+    if N>1:
+        dp[0][1] = dp[1][0] + arr[0][1]
+        dp[1][1] = dp[0][0] + arr[1][1]
 
-    for i in range(2,N):
-        dp[0][i] = max(dp[1][i-1]+arr[0][i], dp[1][i-2]+arr[0][i])
-        dp[1][i] = max(dp[0][i-1]+arr[1][i], dp[0][i-2]+arr[1][i])
+    if N>2:
+        for i in range(2,N):
+            dp[0][i] = max(dp[1][i-1]+arr[0][i], dp[1][i-2]+arr[0][i])
+            dp[1][i] = max(dp[0][i-1]+arr[1][i], dp[0][i-2]+arr[1][i])
     return max(dp[0][N-1], dp[1][N-1])
 
 for _ in range(T):
