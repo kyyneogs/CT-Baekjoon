2de6402c32f81801175b08b15c8b38eb259afd83
kyyneogs <oks990304@gmail.com>
2023-09-04 01:28:36 +0900

    2023-09-04

--- essential/class-5/16946 벽부수고이동ii.py
+++ essential/class-5/16946 벽부수고이동ii.py
@@ -4,21 +4,24 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 board = [[] for _ in range(N)]
+ans_board = [[] for _ in range(N)]
 starting = []
 for i in range(N):
     for k,j in enumerate(input().rstrip()):
         board[i].append(int(j))
+        ans_board[i].append(int(j))
         if j=='1':
             starting.append((i,k))
 
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 visited = [[0 for _ in range(M)] for _ in range(N)]
+group_dict = {}
+que = deque()
 
-def BFS(sx, sy):
-    stack = []
-    que = deque()
+def BFS(sx, sy, group):
+    visited[sx][sy] = group
     que.append((sx,sy))
-    cnt = 0
+    cnt = 1
     while(que):
         x, y = que.popleft()
         for i in range(4):
@@ -27,28 +30,32 @@ def BFS(sx, sy):
                 continue
             if not board[nx][ny] and not visited[nx][ny]:
                 cnt += 1
-                visited[nx][ny] = 1
+                visited[nx][ny] = group
                 que.append((nx, ny))
-                stack.append((nx, ny))
-    for x, y in stack:
-        visited[x][y] = cnt
+    group_dict[group] = cnt
+    return group + 1
 
-def solution(sx, sy):
+def solution(sx, sy, group):
     cost = 1
+    group_stack = set()
     for i in range(4):
         nx, ny = sx+dx[i], sy+dy[i]
         if nx<0 or nx>=N or ny<0 or ny>=M:
             continue
-        if visited[nx][ny]:
-            cost += visited[nx][ny]
-        else:
-            BFS(nx, ny)
-    board[sx][sy] = cost
-    
+        if not visited[nx][ny] and not board[nx][ny]: 
+            group = BFS(nx, ny, group)
+        if visited[nx][ny] and visited[nx][ny] not in group_stack:
+            cost += group_dict[visited[nx][ny]]
+            group_stack.add(visited[nx][ny])
+            
+    ans_board[sx][sy] = cost % 10
+    return group
 
-
+group = 1
 for x,y in starting:
-    solution(x,y)
+    group = solution(x,y, group)
 
-for j in board:
-    print(*j)
+for i in ans_board:
+    for j in i:
+        print(j, end='')
+    print('')
