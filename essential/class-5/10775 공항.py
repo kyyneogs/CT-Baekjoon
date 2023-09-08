628796b322376d301599864f73e5bb6390684402
kyyneogs <oks990304@gmail.com>
2023-09-08 23:45:19 +0900

    2023-09-08

---
+++ essential/class-5/10775 공항.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union_parent(x):
+    parent[x] = x-1
+
+G = int(input())
+P = int(input())
+planes = [int(input()) for _ in range(P)]
+parent = [i for i in range(G+1)]
+result = 0
+for plane in planes:
+    gate = find_parent(plane)
+    if not gate:
+        break
+    union_parent(gate)
+    result += 1
+print(result)
