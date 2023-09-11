c5e9cdd0ba3717e67402c4b66d351f8c67c87713
kyyneogs <oks990304@gmail.com>
2023-09-11 18:46:15 +0900

    2023-09-11

--- essential/class-5/2887 행성터널.py
+++ essential/class-5/2887 행성터널.py
@@ -13,14 +13,19 @@ def union_parent(u, v):
         parent[u] = v
 
 N = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(N)]
+arr = []
+for i in range(N):
+    x, y, z = map(int, input().split())
+    arr.append((x,y,z,i+1))
 graph = []
 parent = [i for i in range(N+1)]
-for i in range(N-1):
-    for j in range(i, N):
-        dis = min(abs(arr[i][0]-arr[j][0]), abs(arr[i][1]-arr[j][1]), abs(arr[i][2]-arr[j][2]))
-        graph.append((i, j, dis))
-graph.sort(key=lambda x:x[2])
+
+for i in range(3):
+    arr.sort(key=lambda x:x[i])
+    for j in range(1,N):
+        dis = min(abs(arr[j-1][0] - arr[j][0]), abs(arr[j-1][1] - arr[j][1]), abs(arr[j-1][2] - arr[j][2]))
+        graph.append((arr[j-1][3], arr[j][3], dis))
+graph.sort(key = lambda x:x[2])
 
 cnt, result = 0, 0
 for u, v, w in graph:
