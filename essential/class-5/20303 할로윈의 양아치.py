318f4573724763c458de398a1ff62acb4de64d46
kyyneogs <oks990304@gmail.com>
2023-09-12 18:06:35 +0900

    2023-09-12

--- essential/class-5/20303 할로윈의 양아치.py
+++ essential/class-5/20303 할로윈의 양아치.py
@@ -1,5 +1,4 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
 def find_parent(x):
@@ -10,19 +9,14 @@ def find_parent(x):
 def union_parent(a, b):
     a, b = (a, b) if a>b else (b, a)
     parent[a] = b
-
-def count_parent(x, cnt=0):
-    if parent[x] != x:
-        visited[x] = cnt+1
-        count_parent(parent[x], cnt+1)
-    else:
-        visited[x] = cnt+1
+    visited[b] += visited[a]
+    arr[b] += arr[a]
 
 N, M, K = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 relation = [tuple(map(int, input().split())) for _ in range(M)]
 parent = [i for i in range(N+1)]
-visited = [0] * (N+1)
+visited = [1] * (N+1)
 
 for a, b in relation:
     a, b = find_parent(a), find_parent(b)
@@ -30,22 +24,34 @@ for a, b in relation:
         union_parent(a, b)
 
 candys = []
-
-for i in range(N, 0, -1):
+for i in range(1,N+1):
     if parent[i] == i:
-        visited[i] += 1
-        candys.append((visited[i], arr[i]))
-    else:
-        visited[i] += 1
-        visited[parent[i]] += 1
-        arr[parent[i]] += arr[i]
+        if visited[i] < K:
+            candys.append((visited[i], arr[i]))
+candys.sort(key=lambda x: (x[0], x[1]))
 
-# candys.sort(key = lambda x: (x[0], x[1]))
 DP = [0] * (K+1)
-
-for i in range(len(candys)):
-    child, candy = candys[i]
-    for j in range(K, child-1, -1):
-        DP[j] = max(DP[j-child] + candy, DP[j])
+for child, candy in candys:
+    for i in range(K, child-1, -1):
+        DP[i] = max(DP[i-child]+candy, DP[i])
 
 print(DP[K-1])
+
+# for i in range(N, 0, -1):
+#     if parent[i] == i:
+#         visited[i] += 1
+#         candys.append((visited[i], arr[i]))
+#     else:
+#         visited[i] += 1
+#         visited[parent[i]] += 1
+#         arr[parent[i]] += arr[i]
+
+# # candys.sort(key = lambda x: x[0])
+# DP = [0] * (K+1)
+
+# for i in range(len(candys)):
+#     child, candy = candys[i]
+#     for j in range(K, child-1, -1):
+#         DP[j] = max(DP[j-child] + candy, DP[j])
+
+# print(DP[K-1])
