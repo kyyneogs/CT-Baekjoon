514d377526549b7b45947b64bf612a39131e71ba
kyyneogs <oks990304@gmail.com>
2023-09-15 15:31:03 +0900

    2023-09-15

--- essential/class-5/1799 비숍.py
+++ essential/class-5/1799 비숍.py
@@ -1,7 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+def check(x, y, flag):
+    dx = x+y
+    dy = 0
+    k = dx - N + 1
+
+    if k > 0:
+        dx -= k
+        dy += k
+    
+    for _ in range(N-abs(k)):
+        check_board[dx][dy] = flag
+        dx -= 1
+        dy += 1
+
+def DFS():
+    pass
+
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-stack = []
-
+check_board = [[True]*N for _ in range(N)]
