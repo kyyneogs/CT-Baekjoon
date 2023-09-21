d8efca7df3bffd33a6c305beb7ff097c2fca67c4
kyyneogs <oks990304@gmail.com>
2023-09-21 21:47:09 +0900

    2023-09-21

---
+++ 알고리즘 분류/백트래킹/15651 N과M(3).py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def backtracking(x, k, length):
+    for i in range(1, x+1):
+        k.append(i)
+        if len(k) >= length:
+            print(' '.join(map(str, k)))
+        else:
+            backtracking(x, k, length)
+        k.pop()
+
+N, M = map(int, input().split())
+backtracking(N, [], M)
