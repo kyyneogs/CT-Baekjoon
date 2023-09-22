fb1644fdb612cdef72d49276b4b6d51811e7ed92
kyyneogs <oks990304@gmail.com>
2023-09-22 20:35:32 +0900

    2023-09-22

--- 알고리즘 분류/백트래킹/14889 스타트와 링크.py
+++ 알고리즘 분류/백트래킹/14889 스타트와 링크.py
@@ -2,38 +2,22 @@ import sys
 from itertools import combinations
 input = sys.stdin.readline
 
-def calculate(buff_link):
+def calculate(a, b):
     result = 0
     for i in range(N//2):
         for j in range(N//2):
-            result += arr[buff[i]][buff[j]]
-            result -= arr[buff_link[i]][buff_link[j]]
+            result += arr[a[i]][a[j]]
+            result -= arr[b[i]][b[j]]
     return abs(result)
 
-def backtracking(depth=0):
-    if depth >= N//2:
-        global mini
-        buff_link = []
-        for i in range(N):
-            if i not in buff:
-                buff_link.append(i)
-        mini = min(mini, calculate(buff_link))
-        return
-
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = 1
-            buff.append(i)
-            backtracking(depth+1)
-            visited[i] = 0
-            buff.pop()
-
 N = int(input())
+sample_list = [i for i in range(N)]
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [0] * N
-buff = []
 mini = int(10e9)
+combi = list(combinations(sample_list, N//2))
 
-backtracking()
+for i in combi:
+    j = list(filter(lambda x: x not in i, sample_list))
+    mini = min(mini, calculate(i, j))
 
 print(mini)
