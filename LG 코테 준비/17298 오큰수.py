41b8ac174aaa92018b26c86c87a9bc8e883743c0
kyyneogs <oks990304@gmail.com>
2023-10-21 12:10:25 +0900

    2023-10-21

--- LG 코테 준비/17298 오큰수.py
+++ LG 코테 준비/17298 오큰수.py
@@ -3,12 +3,11 @@ input = sys.stdin.readline
 
 N = int(input())
 arr = list(map(int, input().split()))
+answer = [-1]*N
 stack = []
-new_arr = [-1]*N
-
 for i in range(N):
     while stack and arr[stack[-1]] < arr[i]:
-        new_arr[stack.pop()] = arr[i]
+        answer[stack.pop()] = arr[i]
     stack.append(i)
 
-print(' '.join(map(str, new_arr)))
+print(" ".join(map(str, answer)))
