411576eeca2ebf302876000fd9dbe6dcd43a332d
kyyneogs <oks990304@gmail.com>
2024-01-07 22:59:40 +0900

    2023-01-07

--- essential/class-2/C++/2108 통계학.cpp
+++ essential/class-2/C++/2108 통계학.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    vector<int> stack;
     int n, t, sumi=0;
-    int mid, maxi=-4001, mini=4001, freq[8001]={0, };
+    int freqMax=0, freqCnt=0, freqIdx;
+    int maxi=-8000, mini=8000, freq[8001]={0, };
     cin >> n;
     t = n;
 
     while(t--){
         int k;
         cin >> k;
+        stack.push_back(k);
         sumi += k;
-        freq[k]++;
+        freq[k+4000]++;
 
         if (maxi < k) maxi = k;
         if (mini > k) mini = k;
-        if (t==n/2) mid = k;
     }
+    for (int i=mini+4000; i<= maxi+4000; i++){
+        if (freq[i] > freqMax) {
+            freqMax = freq[i];
+            freqCnt = 0;
+            freqIdx = i;
+        }
+        else if (freq[i] == freqMax) {
+            freqCnt++;
+            if (freqCnt == 1) freqIdx = i;
+        }
+    }
+    sort(stack.begin(), stack.end());
 
-    cout << round(sumi/n) << "\n";
-    cout << mid << "\n";
-}
+    double avg = sumi / (double)n;
+    cout << (int)round(avg) << "\n";
+    cout << stack[(n-1)/2] << "\n";
+    cout << freqIdx-4000 << "\n";
+    cout << maxi - mini;
+    }
