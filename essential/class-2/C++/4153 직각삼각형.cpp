d0a7b296454fed77d97e992a54b8ae735d5c061f
kyyneogs <oks990304@gmail.com>
2024-01-20 02:15:42 +0900

    2023-01-20

---
+++ essential/class-2/C++/4153 직각삼각형.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int arr[3];
+    while(true){
+        cin >> arr[0] >> arr[1] >> arr[2];
+        sort(arr, arr+3);
+        if (arr[0]==0 && arr[2]==0) break;
+
+        if (arr[0]*arr[0]+arr[1]*arr[1]==arr[2]*arr[2]) cout << "right\n";
+        else cout << "wrong\n";
+
+    }
+}
